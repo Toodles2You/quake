c483e133509de33c041e652874943a525ea1862f
Justin <95296300+Toodles2You@users.noreply.github.com>
2023-10-31 16:01:14 -0700

    Rip out antiquated network menus

--- nq/client/menu.h
+++ nq/client/menu.h
@@ -3,18 +3,6 @@
 #define _MENU_H
 
 //
-// the net drivers should just set the apropriate bits in m_activenet,
-// instead of having the menu code look through their internal tables
-//
-enum
-{
-    MNET_IPX = 1,
-    MNET_TCP,
-};
-
-extern int m_activenet;
-
-//
 // menus
 //
 void M_Init();
